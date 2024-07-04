aeb998acc03da45677dec5a89db694a8989b00b2
HippolyteWeber <hippolyte.weber@gmail.com>
2024-07-05 00:02:30 +0200

    feat: css responsive 75%

--- src/app/components/HeroHeader.jsx
+++ src/app/components/HeroHeader.jsx
@@ -4,26 +4,34 @@ import Image from "next/image";
 
 const HeroHeader = () => {
   return (
-    <div className="flex justify-center hero min-h-screen mb-44">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <p className="py-6 text-left italic font-thin">
-            Bienvenue sur <span className="text-primary">UNDYSIA </span>votre
-            solution de rédaction {"\n"}adapté aux personnes dyslexiques {"\n"}
-            et malvoyantes. {"\n"}
-            Nous vous proposons un accompagnement dans la{"\n"}
-            rédaction de vos CV{"\n"} et lettres de motivation.
-          </p>
-          <Image
-            src="/home.jpeg"
-            alt="home"
-            width={500}
-            height={500}
-            priority
-          />
+    <>
+      <Image
+        src="/UNDYSIA.png"
+        alt="logo"
+        width={300}
+        height={150}
+        className="absolute"
+      />
+      <div className=" text-center flex flex-col md:flex-row justify-center items-center max-w md:pt-24 pt-48">
+        <p className="py-6 text-2xl text-left italic font-thin text-text w-96 md:mx-32">
+          Bienvenue sur <span className="text-primary text-2xl">UNDYSIA </span>
+          votre solution de rédaction {"\n"}adapté aux personnes dyslexiques{" "}
+          {"\n"}
+          et malvoyantes {"\n"}
+          vous permettant de corriger vos différentes {"\n"}rédactions CV,
+          Lettre de motivation et de les{"\n"} télécharger au format PDF
+        </p>
+        <div className="relative top-[200px] right-[350px]">
+          <section id="section10" class="demo">
+            <h1>Scroll Down Button #10</h1>
+            <a href="#thanks">
+              <span></span>Scroll
+            </a>
+          </section>
         </div>
+        <Image src="/home.jpeg" alt="home" width={500} height={500} />
       </div>
-    </div>
+    </>
   );
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,17 @@
+"use client";
+import { useEffect } from "react";
 import CorrectionTool from "./components/CorrectionTool";
 import Footer from "./components/Footer";
 import HeroHeader from "./components/HeroHeader";
+import "aos/dist/aos.css";
+import AOS from "aos";
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  }, []);
   return (
     <main className="bg-background md:w-screen  justify-center items-center">
       <HeroHeader />
